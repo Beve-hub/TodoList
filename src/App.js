4a9bd2570aa0716cef4ae492dc8a788f10af6be0
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-10-20 17:11:19 -0600

    rtr

--- src/App.js
+++ src/App.js
@@ -26,24 +26,39 @@ function App() {
     setTodo(updatedTasks);
   };
 
+ const dataBuilder = (d) => {
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
+  const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+ }
 
   return (
-    <div className=" w-[50rem] h-[40rem] mt-[5rem]  bg-blue-900 justify-center items-center  overflow-">
+    <div className=" w-[50rem] h-[40rem] mt-[5rem]  bg-blue-800 justify-center items-center  overflow-">
     <div className="overflow-hidden">
-    <h1 className="text-2xl font-semibold w-full flex justify-center  text-white pt-[3rem]">Any Plans For Today</h1>
+    <div className="w-full grid justify-center gap-1 bg-blue-900 items-center text-start p-6  ">
+      <div>
+        <h1 className="text-2xl font-semibold   text-white ">Any Plans For Today</h1>
+         <p className='text-md text-gray-400'>{dataBuilder(new Date())}</p>
+      </div>
+    </div>
      <div className="flex justify-center mt-[2rem] ">
       <input type='text' value={value} 
       onChange={(e) => setValue(e.target.value)} 
-      placeholder='Add Task....'/>
-      <button onClick={addTask} className="bg-cyan-500 p-2 ">Add Task</button>
+      placeholder='Add Task....' className="border-slate-200 placeholder-slate-400 px-3 contrast-more:border-slate-400 contrast-more:placeholder-slate-500"/>
+      <button onClick={addTask} className="bg-cyan-500 p-2  text-2xl font-bold">+</button>
     </div>
     
 
     <ul className="w-full grid justify-center items-center text-[white] text-2xl gap-3 pt-[3rem] ">
       {todo.map((list, index) => 
       <li key={index}>
-        <div className="flex gap-20 items-center ">
+        <div className="flex justify-between  gap-10 items-center bg-cyan-500 p-2 rounded-sm">
         <p>{list}</p>
        <div>
        <button onClick={() => editTask(index,prompt('Edit task:', list))}><BiSolidEdit/></button>
